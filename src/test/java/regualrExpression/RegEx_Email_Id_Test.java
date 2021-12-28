aeb96259b414c4845c3932ab52a41035761ab786
saykamstephen@gmail.com <saykamstephen@gmail.com>
2021-12-28 18:08:55 +0530

    Email Program

--- src/test/java/regualrExpression/RegEx_Email_Id_Test.java
+++ src/test/java/regualrExpression/RegEx_Email_Id_Test.java
@@ -7,9 +7,9 @@ public class RegEx_Email_Id_Test {
 	
 	public static void main(String[] args)
 	{
-		Pattern p = Pattern.compile("[a-zA-Z0-9][a-zA-Z0-9\\W]*[.com/.in]");
-		Matcher m = p.matcher("sunnychinni111@gmail.in");
-		if(m.find() && m.group().equals("sunnychinni111@gmail.in"))
+		Pattern p = Pattern.compile("[a-zA-Z0-9][a-zA-Z0-9_.]{7,30}+@[a-z]{5,10}+.[a-z]{2,6}");
+		Matcher m = p.matcher("example.sunnychinni111@yahoo.in");
+		if(m.find() && m.group().equals("example.sunnychinni111@yahoo.in"))
 		{
 			System.out.println("Email Id is Matching");
 		}
